9d0aafe014f8de95ab2567d36f9c7c8697157b96
Fred <911651855@qq.com>
2018-12-12 20:45:29 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="hello">
+    <div class="haha">哈哈哈哈</div>
+  </div>
+</template>
+<style lang="stylus" scoped>
+.hello {
+  .haha {
+    width: 750px;  //75px 等于1rem
+    background:blues;
+  }
+}
+</style>
+
